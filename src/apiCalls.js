490efdf7c77fed92aefe029204211bff3c50cd77
Trey Marshall <marshall.trey92@yahoo.com>
2023-05-11 14:28:57 -0500

    feat: Add initial files and uncommented API call for later use

---
+++ src/apiCalls.js
@@ -0,0 +1,11 @@
+// const fetchData = (path) => {
+//   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`)
+//     .then(response => {
+//       if(!response.ok) {
+//         throw new Error(response.statusText);
+//       }
+//       return response.json();
+//     })
+// }
+
+// export default fetchData;
