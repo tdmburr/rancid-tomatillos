7f873ac214e9a21607c83f1d087b5c0ee6f9e1a8
Trey Marshall <marshall.trey92@yahoo.com>
2023-05-11 15:06:58 -0500

    feat: Initial app set-up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
+import movieData from "../../movieMock.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movie: movieData
+    }
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <header>
+          <h1>Putrid Portabellos</h1>
+        </header>
+        {/* <MovieContainer /> */}
+      </main>
+    )
+  }
 }
+
 
 export default App;
