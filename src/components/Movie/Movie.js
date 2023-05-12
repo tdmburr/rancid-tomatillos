99582cfc9c6cbae9c7ed3236afa795ead3cbf2be
Joshua Martin <ursa8minority@gmail.com>
2023-05-12 15:29:18 -0600

    App css layout and styling

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -6,7 +6,7 @@ const Movie = (props) => {
     <div className="movieCard" id={props.id}>
       <img src={props.movieInfo.poster_path} alt="Neat Movie"></img>
       <h2>{props.movieInfo.title}</h2>
-      <span>{props.movieInfo.average_rating}</span>
+      <span>Average Ratings: {props.movieInfo.average_rating}</span>
     </div>
   )
 }
