f106f0d7e892ac0ca89c694e2f7dda0aab1f92ca
Joshua Martin <ursa8minority@gmail.com>
2023-05-12 17:04:57 -0600

    Create MovieInfo class component refact css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, {Component} from 'react'
 import MovieContainer from '../MovieContainer/MovieContainer'
 import movieData from "../../movieMock.js"
+import MovieInfo from '../MovieInfo/MovieInfo';
 
 class App extends Component {
   constructor() {
@@ -14,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <main className="App">
-        <header>
+        <header className="App-header">
           <h1>Putrid Portabellos</h1>
         </header>
         <MovieContainer movies = {this.state.allMovies}/>
