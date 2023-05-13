2886f137af3de098a4848e2a4c1fa500b903ecde
Joshua Martin <ursa8minority@gmail.com>
2023-05-13 14:57:04 -0600

    Refactor clickSingleMovie function

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -12,16 +12,22 @@ class MovieInfo extends Component {
 
 
   render() {
-    
+
     return (
       <section>
         <img src={this.state.poster_path} alt="Movie Poster" />
         <div className="moreInfo">
-          <p>{this.state.overview}</p>
-          <p>{this.state.average_rating}</p>
-          <p>{this.state.genres}</p>
-          <p>{this.state.tagline}</p>
-          <p>{this.state.overview}</p>
+        <p>{this.state.title}</p>
+        <p>{this.state.backdrop_path}</p>
+        <p>{this.state.release_date}</p>
+        <p>{this.state.overview}</p>
+        <p>{this.state.genres}</p>
+        <p>{this.state.budget}</p>
+        <p>{this.state.revenue}</p>
+        <p>{this.state.runtime}</p>
+        <p>{this.state.tagline}</p>
+        <p>{this.state.average_rating}</p>
+
         </div>
       </section>
     );
@@ -29,3 +35,7 @@ class MovieInfo extends Component {
 }
 
 export default MovieInfo;
+
+
+
+{"movie":{"id":829799,"title":"Paradise City","poster_path":"https://image.tmdb.org/t/p/original//xdmmd437QdjcCls8yCQxrH5YYM4.jpg","backdrop_path":"https://image.tmdb.org/t/p/original//au4HUSWDRadIcl9CqySlw1kJMfo.jpg","release_date":"2022-11-11","overview":"Renegade bounty hunter Ryan Swan must carve his way through the Hawaiian crime world to wreak vengeance on the kingpin who murdered his father.","genres":["Crime","Action","Thriller"],"budget":20000000,"revenue":0,"runtime":93,"tagline":"","average_rating":1}}
