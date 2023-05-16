0882f50b94eb906c7a2d45b1ab135cae3f3f1aa6
Trey Marshall <marshall.trey92@yahoo.com>
2023-05-16 15:51:07 -0500

    refac: Refactor App render using route to display all movies

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
-const fetchAllMovies = () => {
-    return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+const acquireInfo = (path) => {
+    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`)
     .then(response => {
         if(!response.ok) {
           throw new Error("Failed to fetch movies");
@@ -8,18 +8,8 @@ const fetchAllMovies = () => {
      })
   }
   
-  const fetchSingleMovieDetails = (id) => {
-    return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-    .then(response => {
-        if(!response.ok) {
-          throw new Error("Failed to fetch movies");
-      }
-      return response.json()
-     })
-      
-  }
   
-  export {fetchAllMovies, fetchSingleMovieDetails};
+export default acquireInfo;
 
   
 
